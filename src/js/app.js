2c5728a529421d2768a6ec3071a3f0ea9e11ebb2
Walter <waltervodeb@MacBook-Pro-de-Walter.local>
2020-10-06 23:27:56 -0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,34 @@
+/**Variables Globales */
+let MAIN;
+let MODAL_POST;
+let BTN_SHOW_POST;
+let BTN_CANCEL_POST;
+
+
+//Funciones
+const showPostModal=()=>{
+    MAIN.style.display='none';
+    MODAL_POST.style.display='block';
+    setTimeout(()=>{
+        MODAL_POST.style.transform='translateY(0)';
+    },1);
+
+};
+
+const closePostModal=()=>{
+    MAIN.style.display='block';
+    MODAL_POST.style.transform='translateY(100vh)';
+
+};
+
+//Carga de DOM
+window.addEventListener('load',()=>{
+    MAIN = document.querySelector('#main');
+    MODAL_POST=document.querySelector('#modal-post-section');
+    BTN_SHOW_POST=document.querySelector('#btn-upload-post');
+    BTN_SHOW_POST.addEventListener('click',showPostModal);
+    BTN_CANCEL_POST=document.querySelector('#btn-post-cancel');
+    BTN_CANCEL_POST.addEventListener('click',closePostModal);
+
+
+});
